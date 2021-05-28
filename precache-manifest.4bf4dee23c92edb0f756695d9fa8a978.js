8ba6fc5b40f145eb1587be6ca3f67d5b8076a253
amithos <alex.theutos@gmail.com>
2021-05-28 12:35:15 +0300

    Updates

---
+++ precache-manifest.4bf4dee23c92edb0f756695d9fa8a978.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28cbc6a55f2e18463338d77c02fb82a6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e56bed4fff33013acf88",
+    "url": "./static/css/main.4ef30172.chunk.css"
+  },
+  {
+    "revision": "45d6e97bf5fbb92505f3",
+    "url": "./static/js/2.26eff421.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.26eff421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e56bed4fff33013acf88",
+    "url": "./static/js/main.53355488.chunk.js"
+  },
+  {
+    "revision": "de889ebc4391832068f6",
+    "url": "./static/js/runtime-main.311b6371.js"
+  }
+]);
